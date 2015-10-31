832fe3df3550ca4ab6a52384f094666e1c8d5f92
vhsvhs <victorhansonsmith@gmail.com>
2015-10-31 16:15:31 -0700

    More work on settings

--- chipseqbot/config/wsgi.py
+++ chipseqbot/config/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chipseqbot.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chipseqbot.settings.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
