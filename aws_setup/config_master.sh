18dc0045bf6a4c7b776706cdf109c2210087e89e
vhsvhs <victorhansonsmith@gmail.com>
2015-11-01 13:26:07 -0800

    Added the license and the AWS setup scripts.

---
+++ aws_setup/config_master.sh
@@ -0,0 +1,20 @@
+#
+# config_master.sh
+#
+# This script can be launched on a fresh Ubuntu instance. It will
+# setup Nginx, Gunicorn, Django, and all Python libraries
+# necessary to run the chipseqbot program. After running this script,
+# the instance will be hosting chipseqbot at <IP address>.
+#
+# REQUIREMENTS:
+# This script retrieves secret keys from environmental variables.
+# Contact your system admin. to retrieve the appropriate keys.
+#
+#
+# USAGE:
+# 
+# cd chipseqbot
+# sudo source aws_setup/config_master.sh
+
+
+
