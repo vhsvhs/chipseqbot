ed3015caf2343024031ba39cbb13513058e7167a
vhsvhs <victorhansonsmith@gmail.com>
2015-11-03 22:21:56 -0800

    OK, I think the nginx.conf should work now.

--- aws_setup/config_master.sh
+++ aws_setup/config_master.sh
@@ -1,57 +1,31 @@
-#
-# config_master.sh
-#
-# This script can be launched on a fresh Ubuntu instance. It will
-# setup Nginx, Gunicorn, Django, and all Python libraries
-# necessary to run the chipseqbot program. After running this script,
-# the instance will be hosting chipseqbot at <IP address>.
-#
-# REQUIREMENTS:
-# This script retrieves secret keys from environmental variables.
-# Contact your system admin. to retrieve the appropriate keys.
-#
-# USAGE on a fresh Ubuntu machine (first read REQUIREMENTS):
-#
-# sudo apt-get -y install git
-# git clone https://github.com/vhsvhs/chipseqbot
-# cd chipseqbot
-# source aws_setup/config_master.sh --> launches this script
-#
+worker_processes 1;
 
-# Update apt-get
-sudo apt-get -y update
+user nobody nogroup;
+pid /tmp/nginx.pid;
 
-# Install PIP
-sudo apt-get -y install python-pip
+events {
+    worker_connections 1024;
+    accept_mutex off;
+}
 
-# Install and launch virtualenv
-sudo pip install virtualenv
-virtualenv venv
-source venv/bin/activate
+http {
+    include mime.types;
+    default_type application/octet-stream;
+    sendfile on;
 
-# Install Python packages
-sudo pip install django
-sudo apt-get -y install libpq-dev python-dev
-sudo pip install -r requirements/prod.txt
+    error_log /var/log/nginx.error.log;
+    access_log /var/log/nginx.access.log;
 
-# Install Nginx
-# (In Ubuntu, nginx will auto start upon installation)
-sudo apt-get -y install nginx
 
-# Setup the Nginx configuration files
-sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
-sudo cp aws_setup/nginx.conf /etc/nginx/nginx.conf
-sudo cp aws_setup/chipseqbot.conf /etc/nginx/conf.d/chipseqbot.conf
+    server {
+    	listen 80;
+    	server_name example.org;
+    	access_log  /var/log/nginx/example.log;
 
-# Launch Nginx with the new configuration.
-# Because nginx is launched upong installation (in previous steps), 
-# we need to restart nginx to ensure that correct configurations
-# are loaded.
-sudo /etc/init.d/nginx stop
-sudo /etc/init.d/nginx reload
-sudo /etc/init.d/nginx start
-
-# Launch Gunicorn
-cd chipseqbot # change directories to the location where manage.py lives
-gunicorn -w 4 config.wsgi
-
+    	location / {
+        	proxy_pass http://127.0.0.1:8000;
+        	proxy_set_header Host $host;
+        	proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    	}
+    }
+}
