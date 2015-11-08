d10b0ef5a2cdbd38f2b0ec4d90327aab1e7dda7c
vhsvhs <victorhansonsmith@gmail.com>
2015-11-08 11:26:02 -0800

    .

---
+++ chipseqbot/config/views.py
@@ -0,0 +1,12 @@
+# import os
+# from django import forms
+# from django.conf import settings
+# from django.contrib.auth import logout
+# from django.contrib.auth.forms import UserCreationForm
+# from django.http import HttpResponseRedirect
+# from django.shortcuts import render_to_response, render
+# from django.template import RequestContext, Context, loader
+# from phylobot.myforms import *
+# 
+# def main_page(request):
+#     return render_to_response('index.html')
