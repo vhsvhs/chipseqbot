338ae86c94118bb7b49301941477b16008b537b0
vhsvhs <victorhansonsmith@gmail.com>
2015-11-08 11:24:44 -0800

    .

--- chipseqbot/config/urls.py
+++ chipseqbot/config/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/', include('allauth.urls')),
+    #url(r'^accounts/', include('allauth.urls')),
     #url(r'^csbportal/', include('csbportal.urls'))
 )
