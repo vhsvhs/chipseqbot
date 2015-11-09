8e76791974cddfb91afcdc6356cac409ebdb57f4
vhsvhs <victorhansonsmith@gmail.com>
2015-11-08 19:04:49 -0800

    .

--- chipseqbot/csbportal/views.py
+++ chipseqbot/csbportal/views.py
@@ -2,5 +2,7 @@ import os
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+
+@login_required
 def indextest(request):
     return render_to_response("csbportal/index.html", RequestContext(request))
