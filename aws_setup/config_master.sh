3ba7e031b163d370705e7307782d93cf16d22196
vhsvhs <victorhansonsmith@gmail.com>
2015-11-10 10:30:20 -0800

    .

--- aws_setup/config_master.sh
+++ aws_setup/config_master.sh
@@ -24,20 +24,18 @@ sudo apt-get -y update
 # Install PIP
 sudo apt-get -y install python-pip
 
+# (depricated)
 # Install and launch virtualenv
 #sudo pip install virtualenv
 #virtualenv venv
 #source venv/bin/activate
 
+#
 # Install PostgreSQL
+#
 sudo apt-get -y install postgresql postgresql-contrib libpq-dev
 
-# Setup the Postgre SQL database
 sudo -u postgres psql -c "CREATE DATABASE csbotdb"
-
-#sudo -u postgres psql --dbname=csbotdb -c "CREATE USER csbotuser WITH PASSWORD 'csbotpass'"
-#sudo -u postgres psql --dbname=csbotdb -c "ALTER ROLE csbotuser SET timezone TO 'UTC-8'"
-#sudo -u postgres psql --dbname=csbotdb -c "GRANT ALL PRIVILEGES ON DATABASE csbotdb TO csbotuser"
 
 sudo -u postgres psql --dbname=csbotdb -c "CREATE USER django WITH PASSWORD 'csbotpass'"
 sudo -u postgres psql --dbname=csbotdb -c "ALTER ROLE django SET timezone TO 'UTC-8'"
@@ -46,15 +44,17 @@ sudo -u postgres psql --dbname=csbotdb -c "ALTER USER django CREATEDB"
 sudo -u postgres psql --dbname=csbotdb -c "GRANT ALL PRIVILEGES ON DATABASE csbotdb TO django"
 sudo -u postgres psql --dbname=csbotdb -c "ALTER USER CREATEDB"
 
+#
 # Install Python packages
+#
 sudo apt-get -y install python-dev
 sudo pip install -r requirements/prod.txt
 
+#
 # Install Nginx
 # (In Ubuntu, nginx will auto start upon installation)
+#
 sudo apt-get -y install nginx
-
-# Setup the Nginx configuration files
 sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
 sudo cp aws_setup/nginx.conf /etc/nginx/nginx.conf
 
@@ -70,10 +70,14 @@ sudo /etc/init.d/nginx start
 # Setup the project database
 #
 cd chipseqbot # change directories to the location where manage.py lives
-mkdir ../assets
 python manage.py createsuperuser # --> this will prompt you for a new username and password
 python manage.py makemigrations csbportal
 python manage.py migrate # setup the db
+
+#
+# Setup static files
+#
+mkdir ../assets
 python manage.py collectstatic # move css, js, etc. into the appropriate assets folder
 
 #
